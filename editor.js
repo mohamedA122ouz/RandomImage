2a4d75083701f99e3d0236446d6e52db9077e61c
prime11 <studyatmohamed@gmail.com>
2024-07-15 12:57:51 +0300

    node html when it has images inside the folder and also make hint to till what i finished and what else inside the welcome secreen

--- editor.js
+++ editor.js
@@ -1,19 +1,26 @@
 const { exec, execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
-let mysource = fs.readFileSync(path.join(__dirname, "./code.src"),"utf8");
-let dirData = fs.readdirSync(__dirname);
-const images = dirData.filter((el) => {
-    if (el.includes(".png") || el.includes(".jpg") || el.includes(".jpeg")||el.includes(".tiff")||el.includes(".gif"))
-        return `"${el}"`;
-})
-console.log(images);
-mysource = mysource.replace("0x11",JSON.stringify(images));
-fs.writeFileSync(path.join(__dirname,"./randomQ.html"),mysource);
-try{
-    execSync(`explorer ${path.join(__dirname,"./randomQ.html")}`);
+let mysource = fs.readFileSync(path.join(__dirname, "./code.src"), "utf8");
+let dirData = fs.readdirSync(path.join(__dirname, "./images"));
+const images = dirData.map((el) => {
+    if (el.includes(".png") || el.includes(".jpg") || el.includes(".jpeg") || el.includes(".tiff") || el.includes(".gif"))
+        return `./images/${el}`;
+    else
+        return "";
+}).filter((el)=>el);
+if(images.length !== 0){
+    console.log(images);
+    mysource = mysource.replace("0x11", JSON.stringify(images));
+    fs.writeFileSync(path.join(__dirname, "./randomQ.html"), mysource);
+    try {
+        execSync(`explorer ${path.join(__dirname, "./randomQ.html")}`);
+    }
+    catch (ex) {
+        console.log("opened");
+    }
 }
-catch(ex){
-    console.log("opened");
+else{
+    console.log("PLEASE INSERT IMAGES INTO 'images' FOLDER THEN RUN AGAIN!!");
+    setTimeout(()=>{},20000);
 }
-
